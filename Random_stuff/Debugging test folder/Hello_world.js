0c68e798a132e212e5164c371ba6ddd86ea5e756
Jlomb1 <127466981+Jlomb1@users.noreply.github.com>
2023-07-18 16:03:14 -0600

    Updates to some of the programs
    moved files, deleted files, edited files, etc.

---
+++ Random_stuff/Debugging test folder/Hello_world.js
@@ -0,0 +1,8 @@
+var greeting = 'hello, World!';
+console.log(greeting);
+
+var greeting2 = 'welcome to Node.js';
+console.log(greeting2);
+
+var sentence1 = 'Learning how to debug code with the debugger';
+console.log(sentence1);
